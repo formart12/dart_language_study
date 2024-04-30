9a21829c55145b81bbdac6c928afdd231e56528e
formart12 <formart12@naver.com>
2024-04-30 19:26:16 +0900

    oper

---
+++ oper.dart
@@ -0,0 +1,32 @@
+void main() {
+  int a = 1;
+  int b = 2;
+
+  // print(a == b);
+  // print(a > b);
+  // print(a < b);
+
+  // print(a);
+  // a = a + 3;
+  // print(a);
+  // a++;
+  // print(a);
+  // ++a;
+  // print(a);
+
+  // a = a + 3;
+  // a += 3;
+  // a = a + 1;
+  // a += 1;
+  // a++;
+
+  // int c = 3;
+  // print(c++);
+  // print(c);
+  // print(++c);
+
+  int c = 3;
+  print(c--);
+  print(c);
+  print(--c);
+}
