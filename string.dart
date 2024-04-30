530443e9097514ff238c5eee47d2fbf53c61c0fb
formart12 <formart12@naver.com>
2024-04-30 19:00:30 +0900

    string

---
+++ string.dart
@@ -0,0 +1,12 @@
+void main() {
+  String name = "이준호";
+  String hello = "Hello";
+  print(name);
+  print(name[0]);
+  print(name[name.length - 1]);
+  print(name + hello);
+  print(name + "" + hello);
+
+  print("${name} ${hello}"); // $ 는 변수를 불러올때 사용.
+  print("내 이름은 $name 입니다.");
+}
